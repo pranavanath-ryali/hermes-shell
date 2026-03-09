22f1ed2a7bb5bf6d5dbd90f50e177f6c7d4fba37
pranavanath-ryali <dev.nitrostation@gmail.com>
2026-03-09 14:30:42 +0530

    simply volume OSD

--- app.ts
+++ app.ts
@@ -1,10 +1,12 @@
 import app from "ags/gtk4/app"
 import style from "./style.scss"
 import Bar from "./windows/Bar"
+import VolumeOsd from "./windows/osd/VolumeOsd"
 
 app.start({
-  css: style,
-  main() {
-    app.get_monitors().map(Bar)
-  },
+    css: style,
+    main() {
+        app.get_monitors().map(Bar)
+        VolumeOsd()
+    },
 })
--- windows/osd/VolumeOsd.tsx
+++ windows/osd/VolumeOsd.tsx
@@ -1,1 +1,39 @@
-export default () => {};
+import { Astal } from "ags/gtk4"
+import app from "ags/gtk4/app"
+import { timeout } from "ags/time"
+import AstalWp from "gi://AstalWp?version=0.1"
+import { createState } from "gnim"
+
+export default () => {
+    const wp = AstalWp.get_default().get_default_speaker()
+
+    const [visible, setVisible] = createState(false)
+    const [volume, setVolume] = createState(wp.get_volume())
+
+    let timer = timeout(2000, () => setVisible(false));
+    timer.cancel();
+
+    wp.connect("notify::volume", (v) => {
+        setVolume(wp.get_volume())
+        setVisible(true);
+        timer.cancel()
+        timer = timeout(2000, () => setVisible(false));
+    })
+
+    return (
+        <window
+            visible={visible}
+            title={"VolumeOSD"}
+            name="VolumeOSD"
+            anchor={
+                Astal.WindowAnchor.BOTTOM |
+                Astal.WindowAnchor.LEFT |
+                Astal.WindowAnchor.RIGHT
+            }
+            exclusivity={Astal.Exclusivity.IGNORE}
+            application={app}
+        >
+            <label label={volume.as((v) => String(Math.ceil(v * 100)) + "%")} />
+        </window>
+    )
+}
